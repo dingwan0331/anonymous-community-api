f8385be68a735512151bee6e8e9476f5e3317ea7
dingwan <dingwan0331@gmail.com>
2022-09-07 00:43:09 +0900

    feat: Validator 클래스 추가

---
+++ utils/validators.js
@@ -0,0 +1,38 @@
+const { BadRequestError } = require("./errors");
+
+class Validator {
+  constructor(inputObject) {
+    this.config = {
+      password: this.validatePassword,
+      title: this.validateNull,
+      content: this.validateNull,
+      userName: this.validateNull,
+    };
+    for (let i = 0; i < Object.keys(inputObject).length; i++) {
+      const key = Object.keys(inputObject)[i];
+      const outputObject = {};
+      outputObject[key] = inputObject[key];
+
+      this.config[key](outputObject);
+    }
+  }
+  validatePassword({ password }) {
+    const passwordRegex = /^(?=.*\d)[\d]{6,}$/;
+
+    if (!passwordRegex.test(password)) {
+      throw new BadRequestError(`Invalid password`);
+    }
+    return;
+  }
+  validateNull(valueObject) {
+    const value = Object.values(valueObject)[0];
+    const key = Object.keys(valueObject)[0];
+
+    if (!value) {
+      throw new BadRequestError(`Invalid ${key}`);
+    }
+    return;
+  }
+}
+
+module.exports = { Validator };
