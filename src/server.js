763525c66ef07bbb27d8fb4f8b4fdd89ffcf0309
dingwan <dingwan0331@gmail.com>
2022-10-09 23:19:26 +0900

    refactor: mongoDB connect asyc/await 추가

--- src/server.js
+++ src/server.js
@@ -4,12 +4,12 @@ import { SERVER_PORT } from "./config/index.js";
 import { mongoose } from "./models/index.js";
 import { MONGO_URL } from "./config/index.js";
 
-const serverStart = (app) => {
+const serverStart = async (app) => {
   try {
-    mongoose
-      .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-      .then(() => console.log("Successfully connected to mongodb"))
-      .catch((err) => console.error(err));
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     app.listen(SERVER_PORT, () => {
       console.log(`listening on ${SERVER_PORT}!`);
