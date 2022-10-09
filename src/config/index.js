f1cbcfc3ce8ce2a39ab1ecdce4d605158f01cc7d
dingwan <dingwan0331@gmail.com>
2022-10-09 23:32:36 +0900

    refactor: 사용안하는 SECRET_KEY 제거

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { NODE_ENV, SERVER_PORT = 8000, SECRET_KEY } = process.env;
+const { NODE_ENV, SERVER_PORT = 8000 } = process.env;
 
 const mongoEnvSet = {
   development: "DEV",
@@ -14,4 +14,4 @@ const mongoEnv = mongoEnvSet[NODE_ENV];
 
 const MONGO_URL = process.env[`${mongoEnv}_MONGO_URL`];
 
-export { SERVER_PORT, SECRET_KEY, MONGO_URL };
+export { SERVER_PORT, MONGO_URL };
